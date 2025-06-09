c5525e89fb6d129dd65f548c3f5cf4e8ead95d01
architaggarwal786 <architaggarwal.557@gmail.com>
2025-06-09 15:37:42 +0530

    PR

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,11 @@
-function App() {
+ function App() {
   return (
-    <div className="flex justify-center items-center min-h-screen bg-gray-100">
+    <div className="min-h-screen bg-red-100 flex flex-col items-center justify-center">
       <h1 className="text-3xl font-bold text-blue-500">
         Hello Tailwind + Vite!
+      </h1>
+      <h1 className="text-5xl font-bold text-blue-600">
+        Tailwind Works!
       </h1>
     </div>
   );
